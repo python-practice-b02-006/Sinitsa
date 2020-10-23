e9e6d0515a1971abf25df3155bc47954eea0b97c
Sinitsa-Vasiliy <sinitsa.va@phystech.edu>
2020-10-23 10:36:59 +0300

    Have added a gun that can move

--- lab_6/gun.py
+++ lab_6/gun.py
@@ -27,8 +27,35 @@ class Target():
 
 
 class Gun():
-    pass
-
+    def __init__(self, coord=[30, SCREEN_SIZE[1]//2], max_pow=50, min_pow=10, color=RED):
+        self.coord = coord
+        self.angle = 0
+        self.max_pow = max_pow
+        self.min_pow = min_pow
+        self.color = color
+        self.active = False
+        self.pow = min_pow
+        
+    def draw(self, screen):
+        gun_shape = []
+        vec_1 = np.array([int(5*np.cos(self.angle - np.pi/2)), int(5*np.sin(self.angle - np.pi/2))])
+        vec_2 = np.array([int(self.pow*np.cos(self.angle)), int(self.pow*np.sin(self.angle))])
+        gun_pos = np.array(self.coord)
+        gun_shape.append((gun_pos + vec_1).tolist())
+        gun_shape.append((gun_pos + vec_1 + vec_2).tolist())
+        gun_shape.append((gun_pos + vec_2 - vec_1).tolist())
+        gun_shape.append((gun_pos - vec_1).tolist())
+        pg.draw.polygon(screen, self.color, gun_shape)
+    
+    def move(self, inc):
+        if (self.coord[1] > 30 or inc > 0) and (self.coord[1] < SCREEN_SIZE[1] - 30 or inc < 0):
+            self.coord[1] += inc
+            
+    def set_angle(self, target_pos):
+        self.angle = np.arctan2(target_pos[1] - self.coord[1], target_pos[0] - self.coord[0])
+            
+    def strike(self):
+        pass
 
 class Table():
     pass
@@ -41,17 +68,29 @@ class Manager():
 
     def process(self, events, screen):
         done = self.handle_events(events)
+        
+        if pg.mouse.get_focused():
+            mouse_pos = pg.mouse.get_pos()
+            self.gun.set_angle(mouse_pos)
+            
         self.draw(screen)
         return done
     
     def draw(self, screen):
         screen.fill(BLACK)
+        self.gun.draw(screen)
 
     def handle_events(self, events):
         done = False
         for event in events:
             if event.type == pg.QUIT:
                 done = True
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    self.gun.move(-5)
+                elif event.key == pg.K_DOWN:
+                    self.gun.move(5)
+            
             
         return done
 
